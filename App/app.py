569b7f3d18b1f314db3289040398869223f82f12
Faith Shim <faith.a.shim@gmail.com>
2024-03-06 09:48:05 +0000

    change

--- App/app.py
+++ App/app.py
@@ -72,7 +72,7 @@ def index():
   return '<h1>Poke API v1.0</h1>'
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=81)
+  app.run(host='0.0.0.0', port=41945)
 
 
 #Faith Routes-------------------------------------------------------
